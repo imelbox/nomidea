6bd5b5d4f5029711cecd8f1f84788ede7a950c5f
Mohamed Tamsamani <mohamed@tousfacteurs.com>
2021-09-12 03:08:01 +0100

    initial next project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h3>NOMidea</h3>
+      <h4>Notion of Modules idea</h4>
+      <p>
+        Welcome to NOMidea a simple idea of modules in React and TypeScript,
+      </p>
+    </div>
+  );
+}
