f68a9326ee26c2ad0e2274723a61b9836f988402
Mohamed Tamsamani <mohamed@tousfacteurs.com>
2021-09-16 07:24:01 +0100

    new prisma migrate

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,63 +1,63 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
 	log: ['query', 'info', 'warn', 'error'],
 });
 
+const users = [
+	{
+		name: 'admin',
+		email: 'admin@nomidea.com',
+		confirmed: true,
+		profile: {
+			create: {
+				bio: 'I am the admin',
+				avatar: 'https://randomuser.me/api/portraits/men/17.jpg',
+				fullName: 'admin general',
+				role: 'SUPER_ADMIN',
+				links: {
+					create: {
+						provider: 'GITHUB',
+						url: 'https://github.com/imelbox/nomidea',
+					},
+				},
+			},
+		},
+	},
+	{
+		name: 'noadmin',
+		email: 'noadmin@nomidea.com',
+		confirmed: false,
+		profile: {
+			create: {
+				bio: 'I am not the admin',
+				avatar: 'https://randomuser.me/api/portraits/men/27.jpg',
+				fullName: 'user play',
+				role: 'USER',
+				links: {
+					create: {
+						provider: 'GITHUB',
+						url: 'https://github.com/imelbox/nomidea',
+					},
+				},
+			},
+		},
+	},
+];
+
 const main = async () => {
 	console.log('Start seeding...');
 
-	const users = [
-		{
-			name: 'admin',
-			email: 'admin@nomidea.com',
-			confirmed: true,
-			profile: {
-				create: {
-					bio: 'I am the admin',
-					avatar: 'https://randomuser.me/api/portraits/men/17.jpg',
-					fullName: 'admin general',
-					role: 'ADMIN',
-					links: {
-						create: {
-							provider: 'GITHUB',
-							url: 'https://github.com/imelbox/nomidea',
-						},
-					},
-				},
-			},
-		},
-		{
-			name: 'noadmin',
-			email: 'noadmin@nomidea.com',
-			confirmed: false,
-			profile: {
-				create: {
-					bio: 'I am not the admin',
-					avatar: 'https://randomuser.me/api/portraits/men/27.jpg',
-					fullName: 'user play',
-					role: 'USER',
-					links: {
-						create: {
-							provider: 'GITHUB',
-							url: 'https://github.com/imelbox/nomidea',
-						},
-					},
-				},
-			},
-		},
-	];
-
 	try {
-		for (const u of users) {
-			const user = await prisma.user.create({
-				data: u,
-			});
-
-			console.log(`Created user with ID ${user.id}`);
-		}
+		await Promise.allSettled(
+			users.map(async (user) => {
+				console.log(`Seeding user ${user.name}...`);
+				return await prisma.users.create(user);
+			})
+		);
 	} catch (e) {
-		console.error(e);
+		console.log('Seed Error:', e);
 	}
 
 	console.log('Finished seeding.');
@@ -66,5 +66,3 @@ const main = async () => {
 };
 
 main();
-
-console.log('Bottom of script');
